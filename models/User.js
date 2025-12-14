38d8ece2ed0d1acf5dd9feb6000ca23cd5e5e1b2
Abdul Wahab <abdulwahabee5@gmail.com>
2025-12-15 01:12:03 +0500

    made new roles like senior and junior and also added email in footers as well

--- models/User.js
+++ models/User.js
@@ -17,8 +17,8 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     required: true,
-    enum: ['user', 'admin', 'receptionist', 'lab_technologist'],
-    default: "user"
+    enum: ['admin', 'senior_receptionist', 'junior_receptionist', 'senior_lab_tech', 'junior_lab_tech'],
+    default: "junior_receptionist"
   }
 }, { timestamps: true });
 
